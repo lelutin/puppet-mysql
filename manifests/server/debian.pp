d69d43efa5b211da815945695bffa8782081e8af
Gabriel Filion <gabster@lelutin.ca>
2015-05-13 16:03:47 -0400

    mysql_ibdata1 is not managed anymore but is still overridden for debian

--- manifests/server/debian.pp
+++ manifests/server/debian.pp
@@ -3,9 +3,7 @@ class mysql::server::debian inherits mysql::server::clientpackage {
   File['mysql_data_dir'] {
     path => '/var/lib/mysql',
   }
-  File['mysql_ibdata1'] {
-    path => '/var/lib/mysql/ibdata1',
-  }
+
   file { 'mysql_debian_cnf':
     path    => '/etc/mysql/debian.cnf',
     notify  => Service['mysql'],
